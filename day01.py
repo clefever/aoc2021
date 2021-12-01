47c31145bbdea1c54e4fdf663eeb7dbeaddbe8ac
Chris Lefever <linuxcl4@gmail.com>
2021-12-01 00:52:46 -0500

    Refactoring for day 1

--- day01.py
+++ day01.py
@@ -3,34 +3,26 @@ import adventofcode
 
 def part1(input):
     """
-    >>> part1(['199', '200', '208', '210', '200', '207', '240', '269', '260', '263'])
+    >>> part1([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
     7
     """
-    count = 0
-    for i in range(len(input)-1):
-        if int(input[i]) < int(input[i+1]):
-            count += 1
-    return count
+    return sum(1 if input[i] < input[i+1] else 0 for i in range(len(input)-1))
 
 
 def part2(input):
     """
-    >>> part2(['199', '200', '208', '210', '200', '207', '240', '269', '260', '263'])
+    >>> part2([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
     5
     """
-    count = 0
-    for i in range(len(input)-3):
-        a = int(input[i]) + int(input[i+1]) + int(input[i+2])
-        b = int(input[i+1]) + int(input[i+2]) + int(input[i+3])
-        if a < b:
-            count += 1
-    return count
+    window = lambda i, list : list[i] + list[i+1] + list[i+2]
+    return sum(1 if window(i, input) < window(i+1, input) else 0 for i in range(len(input)-3))
 
 
 def main():
     puzzle_input = adventofcode.read_input(1)
-    adventofcode.answer(1, 1581, part1(puzzle_input))
-    adventofcode.answer(2, 1618, part2(puzzle_input))
+    input_nums = [int(num) for num in puzzle_input]
+    adventofcode.answer(1, 1581, part1(input_nums))
+    adventofcode.answer(2, 1618, part2(input_nums))
 
 
 if __name__ == "__main__":
